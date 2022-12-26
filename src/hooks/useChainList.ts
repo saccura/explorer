790c58af6c268d8054bfdf34a5622642c5d59817
Anton Kochetkov <info@enface.io>
2022-12-26 17:26:42 +0700

    261222 home 1

--- src/hooks/useChainList.ts
+++ src/hooks/useChainList.ts
@@ -8,10 +8,9 @@ import React from "react";
 export default function() {
   const [chains, setChains] = React.useState<Chain[]>([
     {
-      name: "POPXChain",
+      name: "NET123",
       network: "13579",
-      rpc: ["https://rpc.popxchain.com/"],
-      // rpc: ["https://rpc.api.moonbeam.network/"],
+      rpc: ["https://rpc.api.moonbeam.network/"],
     },
   ]);
 
